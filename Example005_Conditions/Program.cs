90cd805061e7bc1a87d909b68291d29e7b79fa73
Alexandr <shlyapnikoff2012@gmail.com>
2022-10-12 22:09:31 +0500

    Add Condition

---
+++ Example005_Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "настя")
+{
+    Console.WriteLine("Ура, это же Настя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
